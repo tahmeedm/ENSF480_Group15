7a298a8194b0f909c460b3ddccd74ed42fe8f0cd
XichengW <xicheng.wang@ucalgary.ca>
2024-11-23 19:45:32 -0700

    initialize server.js for Seatbooking and payment.

--- server.js
+++ server.js
@@ -80,6 +80,69 @@ app.post('/RegisteredUser', (req, res) => {//This is the test URL for running Te
   });
 });
 
+app.post('/Seat', (req, res) => {//This is the test URL for running TestProgram.class URL:http://localhost:3000/Seat
+  console.log('Running /Seat');
+  const {modeType, email, username, theater, screen, seatNumber } = req.body;
+
+  if (!modeType) {
+    return res.status(400).send('No inputData provided');
+  }
+
+  if(modeType === "Book"){
+    return res.status(200).send("For Booking, backend need implement.");
+  }
+  // Executing a Java Program
+  // path: ./ENSF480_Domain/bin use public for test.
+  exec(`java -cp ./public Seat ${modeType || 'null'} ${email || 'null'} ${username || 'null'} ${theater || 'null'} ${screen || 'null'} ${seatNumber || 'null'} `, (err, stdout, stderr) => {//This is the command input, and will send output to client.
+    if (err) {
+      res.status(500).send('Error executing Java file: ' + err);
+      return res.status(200).send("For Seats, backend need implement.")
+      return;
+    }
+    if (stderr) {
+      res.status(500).send('stderr: ' + stderr);
+      return;
+    }
+
+    // Returns the output of a Java program
+    res.status(200).send(stdout);
+    // res.status(200).json({ message: 'Java process started' });
+  });
+});
+
+app.post('/Payment', (req, res) => {//This is the test URL for running TestProgram.class URL:http://localhost:3000/Payment
+  console.log('Running /Payment');
+  const {modeType, cardNumber, email, cvv, expiryDate  } = req.body;
+
+  if (!modeType) {
+    return res.status(400).send('No inputData provided');
+  }
+
+  if(modeType === "check"){
+    return res.status(200).send("For payment check, backend need implement.");
+  }
+  if(modeType === "record"){
+    return res.status(200).send("For payment record, backend need implement.");
+  }
+  // Executing a Java Program
+  // path: ./ENSF480_Domain/bin use public for test.
+  exec(`java -cp ./public PaymentInfo ${modeType || 'null'} ${email || 'null'} ${cardNumber || 'null'} ${cvv || 'null'} ${expiryDate || 'null'}`, (err, stdout, stderr) => {//This is the command input, and will send output to client.
+    if (err) {
+      res.status(500).send('Error executing Java file: ' + err);
+      return res.status(200).send("For Payment, backend need implement.")
+      return;
+    }
+    if (stderr) {
+      res.status(500).send('stderr: ' + stderr);
+      return;
+    }
+
+    // Returns the output of a Java program
+    res.status(200).send(stdout);
+    // res.status(200).json({ message: 'Java process started' });
+  });
+});
+
 // Start Server.
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
