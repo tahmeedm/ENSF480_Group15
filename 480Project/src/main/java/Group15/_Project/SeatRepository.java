9c21eb58fea1122d6c9133abda43fd73544e5aac
zzz-Ricky <120093810+zzz-Ricky@users.noreply.github.com>
2024-12-01 15:35:32 -0700

    working seats

--- 480Project/src/main/java/Group15/_Project/SeatRepository.java
+++ 480Project/src/main/java/Group15/_Project/SeatRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface SeatRepository extends JpaRepository<Seat, Long> {
     Optional<Seat> findBySeatNumber(int seatNumber);
 
-    @Query("SELECT s FROM Seat s JOIN s.screening ssl WHERE ssl.id = :screeningId")
+    // @Query("SELECT s FROM Seat s JOIN s.screening ssl WHERE ssl.id = :screeningId")
     List<Seat> findByScreening(Screening screening);
 
     List<Seat> findByOccupant(User occupant);
